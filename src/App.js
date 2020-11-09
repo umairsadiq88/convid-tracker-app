acbce9abd50d508b439d17f2b8e5bf1615bb3715
umairsadiq88 <umairsadiq2010@gmail.com>
2020-11-09 17:53:19 +0500

    start covid app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from 'react';
 import './App.css';
-import Cards from "./components/card/Card"
-import ButtonAppBar from "./components/Nav/Nav"
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+import { DataFetching } from "./api";
+
+
 
 
 function App() {
-  return (
-    <div className="App">
-      <ButtonAppBar />
 
-      <h1>Covid-19</h1>
+
+  return (
+
+    <>
+    <div className="container">
+
+
+     <h1>Covid-19</h1>
+      <ul>
+     [posts.map(post => (
+       <li key= {post.id}> {post.title}</li>
+     ))]
+
+    </ul>
 
       <Cards />
-
+      <Chart />
+      <CountryPicker />
 
     </div>
+    </>
   );
 }
 
