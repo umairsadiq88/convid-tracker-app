1d546ab6a59cfab384a558bfefdb056a793f4aaa
umairsadiq88 <umairsadiq2010@gmail.com>
2020-11-10 18:48:48 +0500

    covid App functional

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import './App.css';
-import Cards from "./components/Cards/Cards";
-import Chart from "./components/Chart/Chart";
-import CountryPicker from "./components/CountryPicker/CountryPicker";
+import React from 'react'
+import Cards from './components/Cards/Cards'
 
-import { DataFetching } from "./api";
-
-
-
-
-function App() {
-
-
+const App = () => {
   return (
-
-    <>
-    <div className="container">
-
-
-     <h1>Covid-19</h1>
-      <ul>
-     [posts.map(post => (
-       <li key= {post.id}> {post.title}</li>
-     ))]
-
-    </ul>
-
+    <div>
+      
       <Cards />
-      <Chart />
-      <CountryPicker />
-
     </div>
-    </>
-  );
+  )
 }
+
 
 export default App;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,110 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
 
 const Cards = () => {
+
+    const [totalcases, setTotalCases] = useState([]);
+    const [activecases, setActiveCases] = useState([]);
+    const [deaths, setDeaths] = useState([]);
+    const [recovered, setRecovered] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
+            const res = await axios.get('https://api.quarantine.country/api/v1/summary/latest')
+            console.log(res.data.data.summary.total_cases);
+
+            setTotalCases(res.data.data.summary.total_cases);
+            setActiveCases(res.data.data.summary.active_cases);
+            setDeaths(res.data.data.summary.deaths);
+            setRecovered(res.data.data.summary.recovered);
+
+        }
+
+        getData();
+    });
+
+    // .catch(err => {
+    //     console.log(err)
+    // })
+
+    const classes = useStyles();
+    
+
     return (
-        <div>
-            <h1>Cards</h1>
-        </div>
+        <>
+
+            <div>
+                <h1>COVID TRACKER</h1>
+            </div>
+
+
+            <Card className={classes.root} variant="outlined">
+
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Total Cases
+        </Typography>
+                    <Typography variant="h5" component="h2">
+                        {totalcases}
+                    </Typography>
+                </CardContent>
+
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Active Cases
+        </Typography>
+                    <Typography variant="h5" component="h2">
+                        {activecases}
+                    </Typography>
+                </CardContent>
+
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Deaths
+        </Typography>
+                    <Typography variant="h5" component="h2">
+                        {deaths}
+                    </Typography>
+                </CardContent>
+
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom> Recovered   </Typography>
+
+                    <Typography variant="h5" component="h2"> {recovered}     </Typography>
+                </CardContent>
+
+            </Card>
+
+        </>
     )
-}
+
+};
 
 export default Cards;
+
+
+
